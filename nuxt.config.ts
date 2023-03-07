8e15cf89d9392d05354f9ac17be0e8426628aa9a
Ysabel Fuentes <ysabel.fuentesp@gmail.com>
2023-03-07 10:15:52 +0100

    Incuyendo modulo imagen
    Se incluye modulo de imagen para optimizacion de landing page. Se
    trabaja con la etiqueta nuxt-img.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+
 export default defineNuxtConfig({
 
   app: {
@@ -21,18 +22,22 @@ export default defineNuxtConfig({
       ],
       script: [
         /* JavaScript Libraries */
-        { src:"https://code.jquery.com/jquery-3.4.1.min.js" },
-        { src:"https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js" },
-        { src:"https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js", integrity:"sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13", crossorigin:"anonymous" },
+        { src: "https://code.jquery.com/jquery-3.4.1.min.js" },
+        { src: "https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js" },
+        { src: "https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js", integrity: "sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13", crossorigin: "anonymous" },
 
-         /* Parallaxs Libraries */
-        { src:"https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js" },
-        { src:"https://cdn.jsdelivr.net/parallax.js/1.4.2/parallax.min.js"},
+        /* Parallaxs Libraries */
+        { src: "https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js" },
+        { src: "https://cdn.jsdelivr.net/parallax.js/1.4.2/parallax.min.js" },
       ],
     }
   },
 
   /* Template Stylesheet */
   css: ['@/assets/css/style.css', '@/assets/css/bootstrap.min.css'],
-
+  
+  /* nuxt/image-edge borde */
+  modules: [
+    '@nuxt/image-edge',
+]
 })
