a634bda536aa1649da63868bd0474998f5583834
Ysabel Fuentes <ysabel.fuentesp@gmail.com>
2023-03-08 23:31:36 +0100

    actualizacion jquery

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -30,8 +30,8 @@ export default defineNuxtConfig({
         { src: "https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js", integrity: "sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13", crossorigin: "anonymous" },
 
         /* Parallaxs Libraries 
-        { src: "https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js" },*/
-        { src: "https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js" },
+        { src: "https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js" }, */
+        { src: "https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js" },
         { src: "https://cdn.jsdelivr.net/parallax.js/1.4.2/parallax.min.js" },
       ],
     }
