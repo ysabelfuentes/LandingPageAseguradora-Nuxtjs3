ac86f87695fff8375d6681efaf39830637cf284d
Ysabel Fuentes <ysabel.fuentesp@gmail.com>
2023-03-09 00:31:15 +0100

    optimizando las fuentes

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -14,11 +14,14 @@ export default defineNuxtConfig({
         { name: 'description', content: 'My first nuxt site.' },
       ],
       link: [
-        /* Google Web Fonts */
+        /* Google Web Fonts 
         { rel: 'preconnect', href: 'https://fonts.googleapis.com', crossorigin: 'anonymous' },
-        { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: 'anonymous' },
-        { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Jost:wght@500;600;700&family=Open+Sans:wght@400;500&display=swap' },
-        /* Icon Font Stylesheet */
+        { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: 'anonymous' }, */
+        { rel: 'preload', href: 'https://fonts.googleapis.com', crossorigin: 'anonymous' },
+        { rel: 'preload', href: 'https://fonts.gstatic.com', crossorigin: 'anonymous' },
+        { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Roboto:400,700&display=swap' },
+        /*{ rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Jost:wght@500;600;700&family=Open+Sans:wght@400;500&display=swap' },
+       Icon Font Stylesheet */
         { rel: 'stylesheet', href: 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css' },
         { rel: 'stylesheet', href: 'https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css' },
         { rel: 'stylesheet', href: 'https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css', integrity: 'sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3', crossorigin: 'anonymous' }
